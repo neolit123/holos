cb4da40de6db4ad84374c363dd6f197783c1557a
Lubomir I. Ivanov <neolit123@gmail.com>
2011-02-12 03:51:42 +0200

    sync svn r83
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- holos.h
+++ holos.h
@@ -28,17 +28,18 @@
 // platform
 
 #ifdef H_LINUX
-  #include "h/h_Linux.h"
+  //#include "h/h_Linux.h"
+  #include "h/h_Platform_Linux.h"
 #endif
 
 #ifdef H_WIN32
-  #include "h/h_Win32.h"
+  //#include "h/h_Win32.h"
+  #include "h/h_Platform_Win32.h"
 #endif
 
-
 #include "h/h_Host.h"
-//#include "h/h_Format.h"
 #include "h/h_Instance.h"
+//#include "h/h_Format.h"
 
 #include "h/h_Parameter.h"
 #include "h/h_Descriptor.h"
@@ -46,15 +47,24 @@
 // host, instance, format
 
 #ifdef H_EXE
-  #include "h/h_Exe.h"
+  //#include "h/h_Exe.h"
+  #include "h/h_Host_Exe.h"
+  #include "h/h_Instance_Exe.h"
+  #include "h/h_Format_Exe.h"
 #endif
 
 #ifdef H_LADSPA
-  #include "h/h_Ladspa.h"
+  //#include "h/h_Ladspa.h"
+  #include "h/h_Host_Ladspa.h"
+  #include "h/h_Instance_Ladspa.h"
+  #include "h/h_Format_Ladspa.h"
 #endif
 
 #ifdef H_VST
-  #include "h/h_Vst.h"
+  //#include "h/h_Vst.h"
+  #include "h/h_Host_Vst.h"
+  #include "h/h_Instance_Vst.h"
+  #include "h/h_Format_Vst.h"
 #endif
 
 #include "h/h_Editor.h"
